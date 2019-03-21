e74e22ee3345785fc5f4eaf74287636dec396411
Marc Mendez <marcmendezroca@gmail.com>
2019-03-21 23:34:50 +0100

    Almost Final Version

--- Script.R
+++ Script.R
@@ -1,67 +1,139 @@
-library(readxl)
-Russet_ineqdata <- read.table("Data-File-and-Script-posting-Task/Russet_ineqdata.txt", sep= '\t')
-Russet_ineqdata$demo <- as.factor(Russet_ineqdata$demo)
-levels(Russet_ineqdata$demo) = c("Stable", "Instable", "Dictatorship")
 library(mice)
+source("PCA.R")
 
-#Imputation By Mice And defined X as matrix with continous values.
-imputed_data <- mice(Russet_ineqdata)
-imputed_data <- complete(imputed_data)
-row.names(imputed_data) <- row.names(Russet_ineqdata)
-X <- as.matrix(imputed_data[,0:8])
-
-
-
-#a. Define the matrix N of weights of individuals (with uniform weights).
-weights <- rep(1,nrow(X))
-N <- diag(weights/sum(weights))
-#b. Compute the centroid G of individuals.
-G <-  colMeans(X)
-#c. Compute the covariance or correlation matrix of X (be aware of dividing by sum(weights_i)).
-X.cov <- cov(X)/sum(weights)
-X.centered <- scale(X, scale=FALSE)
-X.cov.manual <- (t(X.centered)%*%N%*%X.centered)/sum(weights)
-#View(X.cov - X.cov.manual)
-
-X.cor <- cor(X)/sum(weights)
-X.standarized <- scale(X, center = FALSE, scale=TRUE)
-X.cor.manual <- (t(X.standarized)%*%N%*%X.standarized)/sum(weights)
-#View(X.cor - X.cor.manual)
-
-#d. Compute the centered X matrix and standardized X matrix.
-X.centered <- scale(X, scale=FALSE)
-X.standarized <- scale(X, center = FALSE, scale=TRUE)
-#e. Diagonalize X centered and standarized .
-
-X.centered.eig <- eigen(t(X.centered)%*%N%*%X.centered)
-X.centered.eig$values
-
-X.standarized.eig <- eigen(t(X.standarized)%*%N%*%X.standarized)
-X.standarized.eig$values
-#f. Do the screeplot of the eigenvalues and define the number of significant dimensions. 
-# How much is the retained information?
-plot(X.centered.eig$values, type = "l")
-plot(X.standarized.eig$values, type = "l")
-#g. Compute the projections of individuals in the significant dimensions.
-
-#h. Compute the projection of variables in the significant dimensions.
-
-#i. Plot the individuals in the first factorial plane of Rp. Color the individuals according the ???demo??? variable.
-#j. Plot the variables (as arrows) in the first factorial plane of Rn.
-#k. According to the Russet complete data, justify which metric M is appropriate for this problem.
-#l. Compute the correlation of the variables with the significant principal components and interpret them.
- 
-
-source("Data-File-and-Script-posting-Task/PCA.R")
-
-
+X <- read.table('Russet_ineqdata.txt', header=T, sep='\t', row.names=1)
+X$demo <- as.factor(X$demo)
+levels(X$demo) = c("Stable", "Instable", "Dictatorship")
+md.pattern(X)
+imputedX <- complete(mice(X))
+row.names(imputedX) <- row.names(X)
+X.matrix <- as.matrix(imputedX[,0:8])
 weights <- rep(1,nrow(X.matrix))
 weights <- weights/sum(weights) #Normalized 
 N <- diag(weights) 
-Cor.Normal <- PCA(X.matrix, weights, eucledian=TRUE)
- 
+G = vector()
+for (i in 1:ncol(X.matrix)){
+  G = c(G,weighted.mean(X.matrix[,i],weights))
+}
+G
+X.centered <- X.matrix - rep(G, rep.int(nrow(X.matrix), ncol(X.matrix)))
+X.covariance = (t(X.centered)%*%N%*%X.centered)/sum(weights)
+
+X.standarized <- scale(X.matrix)
+X.correlation <- (t(X.standarized)%*%N%*%X.standarized)/sum(weights)
+
+normalized <- TRUE
+
+if (normalized) {
+  X <- X.standarized
+  S <- X.correlation
+} else {
+  X <- X.centered
+  S <- X.covariance
+}
+X.centered <- X - rep(G, rep.int(nrow(X), ncol(X)))
+X.standarized <- scale(X, center = FALSE, scale=TRUE)
+X.svd <- svd(S)
+X.values <- X.svd$d
+X.vector <- X.svd$u
+plot(X.values, type = "l", main="Screeplot of Russet")
+percentage <- ((X.values[1]+X.values[2])/sum(X.values))
+percentage
+PSI <- X %*% X.vector
+PHI <- sqrt(X.values)*X.vector
+
+plot(PSI, col=c('green', 'blue', 'red')[imputedX$demo])
+legend(x="topright", legend=c('Stable', 'Instable', 'Dictatorship'), fill=c('green', 'blue', 'red'))
+
+originx <- rep(0, ncol(PHI))
+originy <- rep(0, ncol(PHI))
+X = imputedX[,1:8]
+etiq = names(X)
+plot(PHI, xlim=c(0,0), ylim=c(0,0))
+text(PHI,labels=etiq, col="black")
+arrows(originx, originy, PHI[,1], PHI[,2])
+abline(h=0,v=0,col="gray")
+
+
+COR <-cor(X, PSI)
+COR
+
+
 weights <- rep(1,nrow(X.matrix))
 weights[11] <-0
-weights <- weights/sum(weights) #Normalized 
-N <- diag(weights) 
+weights <- weights/sum(weights) #Normalized
 Cor.Cuba <- PCA(X.matrix, weights, eucledian=TRUE)
+Cor.Cuba
+
+
+
+X.FactoMineR <- imputedX
+pca <- FactoMineR::PCA(graph=T, X.FactoMineR, ncp=8, quali.sup=9)
+plot(pca, cex=0.8, habillage='demo')
+
+
+firstFactorialPlane <- sort(abs(pca$ind$cos2[,1])+abs(pca$ind$cos2[,2]), decreasing=TRUE)
+bestCountry <- firstFactorialPlane[1]
+worseCountry <- firstFactorialPlane[length(firstFactorialPlane)]
+bestCountry
+worseCountry
+
+contribPCA <- pca$ind$contrib
+contribPCA.1 <- sort(abs(contribPCA[,1]), decreasing=TRUE)
+contribPCA.2 <- sort(abs(contribPCA[,2]), decreasing=TRUE)
+bestCountries.1 <- contribPCA.1[1:3]
+bestCountries.2 <- contribPCA.2[1:3]
+bestCountries.1
+bestCountries.2
+
+
+variableRepresented <- sort(abs(pca$var$cos2[,1])+abs(pca$var$cos2[,2]), decreasing=TRUE)
+best <- variableRepresented[1]
+worse <- tail(variableRepresented, n=1)
+best
+worse
+
+firstPrincipalComponent <- sort(abs(pca$var$contrib[,1]), decreasing=TRUE)
+secondPrincipalComponent <- sort(abs(pca$var$contrib[,2]), decreasing=TRUE)
+mostInfluencingVariablesFirst <- firstPrincipalComponent[1:3]
+mostInfluencingVariablesSecond <- secondPrincipalComponent[1:3]
+mostInfluencingVariablesFirst
+mostInfluencingVariablesSecond
+
+significantModalitiesDemo <- sort(abs(pca$quali.sup$cos2[,1]) + abs(pca$quali.sup$cos2[,2]), decreasing=TRUE)
+significantModalitiesDemo
+
+library(nipals)
+nipals <- nipals(X.standarized, 3)
+scores <- nipals$scores
+loadings <- nipals$loadings
+biplot(scores, loadings)
+
+
+PSI = pca$ind$coord[,1:2]
+PHI = pca$var$coord[,1:2]
+PC.ROT <- varimax(PHI)
+PHI.ROT = PC.ROT$loadings[1:ncol(X),]
+X = imputedX[,1:8]
+Xs = scale(X)
+iden = row.names(X)
+etiq = names(X)
+lmb.rot = diag(t(PC.ROT$loadings) %*% PC.ROT$loadings)
+sum(lmb.rot)
+sum(pca$eig[1:2,])
+Psi_stan.rot = Xs %*% solve(cor(X)) %*% PHI.ROT
+PSI.ROT = Psi_stan.rot %*% diag(sqrt(lmb.rot))
+library(calibrate)
+ze = rep(0,ncol(X))
+plot(PHI.ROT,type="n",xlim=c(-1,1),ylim=c(-1,1))
+text(PHI.ROT,labels=etiq, col="blue")
+arrows(ze, ze, PHI.ROT[,1], PHI.ROT[,2], length = 0.07,col="blue")
+abline(h=0,v=0,col="gray")
+circle(1)
+
+plot(PSI.ROT,type="n")
+text(PSI.ROT,labels=iden,col=as.numeric(imputedX$demo))
+abline(h=0,v=0,col="gray")
+
+pca$ind$coord <- pca$ind$coord[,1:2] %*% PC.ROT$rotmat
+FactoMineR::dimdesc(pca, axes=1:2)
